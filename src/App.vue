b403cf4669679d42cadd7d99b8514f90abc95f51
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-22 21:35:59 -0600

    added all files - initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Flexbox</h1>
+    <div class="container flexbox">
+      <div class="box flex-1"></div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
   },
 };
 </script>
 
 <style>
+@import './assets/css/flexbox-util.css';
+
+* {
+  margin: 0;
+}
+
+html, body {
+  width: 100vw;
+  height: 100vh;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  width: 100%;
+  height: 100%;
+}
+
+.box {
+  height: 30vh;
 }
 </style>
