ad8b81262191e247f66f0437cd7b7a22d8a57a27
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-24 16:29:33 -0600

    added - main Conatiner component

---
+++ src/components/main/Container.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container flex-1">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container {
+  background: var(--brightGreen);
+}
+</style>
