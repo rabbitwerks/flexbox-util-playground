b315b828a80ed5eabec523f89ee8ad4c6e120e99
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-24 16:27:35 -0600

    added - routes for playground, getting started

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,22 @@
+import Home from './views/Home.vue';
+import GettingStarted from './views/GettingStarted.vue';
+import Playground from './views/Playground.vue';
+
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: 'Home',
+    component: Home,
+  },
+  {
+    path: '/getting-started',
+    name: 'Getting Started',
+    component: GettingStarted,
+  },
+  {
+    path: '/playground',
+    name: 'Playground',
+    component: Playground,
   },
 ];
 
