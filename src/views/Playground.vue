aabb09c6f6d3a8547254dde8393a137e997f42e4
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-24 16:30:24 -0600

    added - playgound view

---
+++ src/views/Playground.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flexbox flexdir-col">
+    <pg--display class="flex-10"></pg--display>
+    <pg--control-panel class="flex-5"></pg--control-panel>
+  </div>
+</template>
+
+<script>
+import Display from '../components/playground/Display';
+import ControlPanel from '../components/playground/ControlPanel'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    'pg--display': Display,
+    'pg--control-panel': ControlPanel
+  }
+}
+</script>
+
+<style>
+
+</style>
