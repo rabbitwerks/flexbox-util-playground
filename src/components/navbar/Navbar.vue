f2c034ae72c8433379856b870f9a05f6044044ca
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-24 16:29:05 -0600

    added - Navbar component

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar">
+    <h3>navbar</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.navbar {
+  height: 4rem;
+  background-color: var(--navbarBlack);
+}
+</style>
