59ac24b808a6cf5b68d7d672b4b5853e98fe6a71
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-24 23:41:38 -0600

    updated - navbar brand, items

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,6 +1,17 @@
 <template>
-  <div class="navbar">
-    <h3>navbar</h3>
+  <div class="navbar flexbox">
+    <div class="nav-item--brand flex-1">
+      Flexbox Playground
+    </div>
+    <div class="nav-item flexbox flex-center">
+      <span>Home</span>
+    </div>
+    <div class="nav-item flexbox flex-center">
+      <span>Playground</span>
+    </div>
+    <div class="nav-item flexbox flex-center">
+      <span>Getting Started</span>
+    </div>
   </div>
 </template>
 
@@ -14,5 +25,9 @@ export default {
 .navbar {
   height: 4rem;
   background-color: var(--navbarBlack);
+}
+.nav-item {
+  width: 10rem;
+  border-left: 1px solid #fff;
 }
 </style>
