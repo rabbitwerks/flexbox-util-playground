abdde1fa76b9f1765de05ccdd05ae6e6116c0954
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-25 06:25:45 -0600

    added - navitem component with router link and props

---
+++ src/components/navbar/NavItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <router-link :to="routerLinkTo" class="nav-item flexbox flex-center">
+    <span class="nav-item--text">{{ text }}</span>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: ['routerLinkTo', 'text']
+}
+</script>
+
+<style scoped>
+.nav-item {
+  width: 10rem;
+  border-left: 1px solid #fff;
+  text-decoration: none;
+}
+.nav-item--text {
+  color: var(--mainTurq);
+}
+.nav-item--text:active {
+  color: var(--brightGreen);
+  font-weight: 400;
+}
+</style>
