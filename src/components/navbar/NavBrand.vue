b3c877d908edf7440c3e5a115e3d9e8414952f00
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-25 06:25:18 -0600

    added - navbrand component with props

---
+++ src/components/navbar/NavBrand.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nav-item--brand flex-1 flex-align-center align-self">
+    <span>{{ text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['text']
+}
+</script>
+
+<style scoped>
+.nav-item--brand {
+  padding-left: 1rem;
+}
+</style>
