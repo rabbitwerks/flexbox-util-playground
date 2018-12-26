0b26099bb3ab6aaaf45cea0ff7b4ec5cc85501eb
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-25 22:19:37 -0600

    updated - working on layout for playground components

--- src/views/Playground.vue
+++ src/views/Playground.vue
@@ -1,26 +1,50 @@
 <template>
-  <div class="flexbox flexdir-col">
-    <pg--display class="flex-10"></pg--display>
-    <pg--control-panel class="flex-5"></pg--control-panel>
+  <div class="playground flexbox flexdir-col">
+    <pg--display :boxGroup="boxGroup"></pg--display>
+    <pg--control-panel></pg--control-panel>
   </div>
 </template>
 
 <script>
+import eventBus from '../eventbus.js';
 import Display from '../components/playground/Display';
 import ControlPanel from '../components/playground/ControlPanel'
 export default {
   data () {
     return {
-
+      boxGroup: [
+        {
+          name: 'Box',
+          flex: 1,
+        }
+      ]
     }
   },
   components: {
     'pg--display': Display,
     'pg--control-panel': ControlPanel
+  },
+  methods: {
+    addBoxToGroup() {
+      this.boxGroup.push({ name: 'Box', flex: 1 })
+    },
+    removeBoxFromGroup() {
+      this.boxGroup.splice(0, 1)
+    }
+  },
+  mounted() {
+    eventBus.$on('addBoxToGroup', () => {
+      this.addBoxToGroup()
+    });
+    eventBus.$on('removeBoxFromGroup', () => {
+      this.removeBoxFromGroup()
+    });
   }
 }
 </script>
 
 <style>
-
+.playground {
+  height: 100%;
+}
 </style>
