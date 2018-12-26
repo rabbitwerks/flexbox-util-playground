c041211d23ff410764ea3cf92de39682a074654d
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-25 22:20:27 -0600

    updated - passing disp--box as v-for from playground view data

--- src/components/playground/Display.vue
+++ src/components/playground/Display.vue
@@ -1,10 +1,18 @@
 <template>
-  
+  <div id="display" class="flex-10">
+    <disp--box v-for="(box, i) in boxGroup" :key="i"></disp--box>
+
+
+  </div>
 </template>
 
 <script>
+import Display__Box from './Display__Box'
 export default {
-
+  props: ['boxGroup'],
+  components: {
+    'disp--box': Display__Box,
+  }
 }
 </script>
 
