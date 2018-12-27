4760d172bc996c1fa889eefef96974d135cba6af
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-26 23:55:31 -0600

    renamed / refactored

---
+++ src/components/playground/display/Display__FlexItem_Basic.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="box flex-1"></div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.box {
+  height: 100%;
+  background-color: var(--mainTurq);
+  border-left: 1px solid var(--backgroundGrey)
+}
+</style>
