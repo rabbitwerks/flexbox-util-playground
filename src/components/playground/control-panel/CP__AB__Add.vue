1ebff7172a523dd033b4288812237d036bf65ff3
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-26 23:57:36 -0600

    added - add button cmp

---
+++ src/components/playground/control-panel/CP__AB__Add.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="addItemToGroup">Add Flex Item</button>
+</template>
+
+<script>
+import eventBus from '../../../eventbus.js';
+export default {
+  methods: {
+    addItemToGroup() {
+      eventBus.$emit('addItemToGroup')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
