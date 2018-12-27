a2d0863796edab3af78f5d2c1105c66d981cd281
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-26 23:57:50 -0600

    added - remove button cmp

---
+++ src/components/playground/control-panel/CP__AB__Remove.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="removeItemFromGroup">Remove Flex Item</button>
+</template>
+
+<script>
+import eventBus from '../../../eventbus.js'
+export default {
+  methods: {
+    removeItemFromGroup() {
+      eventBus.$emit('removeItemFromGroup')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
