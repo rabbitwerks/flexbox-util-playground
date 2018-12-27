89d84d496e3a989c992bee11f75f1cfa9658a9b0
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-26 23:57:05 -0600

    refactored for components

---
+++ src/components/playground/control-panel/ControlPanel.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex-5 flexbox">
+    <cp--action-buttons></cp--action-buttons>
+    <cp--flex-input-panel></cp--flex-input-panel>
+  </div>
+</template>
+
+<script>
+import eventBus from '../../../eventbus.js';
+import CP__ActionButtons from './CP__ActionButtons';
+import CP__FlexInputPanel from './CP__FlexInputPanel';
+export default {
+  components: {
+    'cp--action-buttons': CP__ActionButtons,
+    'cp--flex-input-panel': CP__FlexInputPanel
+  }
+}
+</script>
+
+<style>
+
+</style>
