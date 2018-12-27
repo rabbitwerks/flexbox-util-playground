9b6f273fff6a3befbc532e7101484a7cf62e94e6
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-26 23:56:03 -0600

    renamed / refactored for flex item group

---
+++ src/components/playground/display/Display.vue
@@ -0,0 +1,34 @@
+<template>
+  <div 
+    id="display--wrapper" 
+    class="flex-10">
+      <div 
+        id="display--content"
+        class="flexbox flexdir-row ">
+          <disp--flex-item--basic 
+            v-for="(flexItem, i) in flexItemGroup" 
+            :key="i">
+          </disp--flex-item--basic>
+      </div>
+  </div>
+</template>
+
+<script>
+import Display__FlexItem_Basic from './Display__FlexItem_Basic'
+export default {
+  props: ['flexItemGroup'],
+  components: {
+    'disp--flex-item--basic': Display__FlexItem_Basic,
+  }
+}
+</script>
+
+<style scoped>
+#display--wrapper {
+  border: 1rem solid var(--darkTurq)
+}
+#display--content {
+  height: 100%;
+}
+
+</style>
