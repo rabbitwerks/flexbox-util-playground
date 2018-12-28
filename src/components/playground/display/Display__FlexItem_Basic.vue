27b02f0dba3a59cd194ea148611f6c97cf779fcd
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-27 23:46:56 -0600

    updated - flexItem prop, dynamic styling for flex amount, removeSelf event/method

--- src/components/playground/display/Display__FlexItem_Basic.vue
+++ src/components/playground/display/Display__FlexItem_Basic.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="box flex-1"></div>
+  <div 
+    :style="{ 'flex': flexItem.flex }"
+    @dblclick="removeSelf"
+    class="flex-item--basic">
+  </div>
 </template>
 
 <script>
+import eventBus from '../../../eventbus.js'
 export default {
-
+  props: ['flexItem', 'index'],
+  methods: {
+    removeSelf() {
+      eventBus.$emit('removeSelfFromGroup', this.index)
+    }
+  }
 }
 </script>
 
 <style>
-.box {
+.flex-item--basic {
   height: 100%;
   background-color: var(--mainTurq);
   border-left: 1px solid var(--backgroundGrey)
