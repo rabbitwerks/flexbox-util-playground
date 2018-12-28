9974fcbfee4529cef3b6bd0d04ee467d522a1224
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-27 23:47:37 -0600

    updated - passing flexitemgroup data as props

--- src/components/playground/control-panel/ControlPanel.vue
+++ src/components/playground/control-panel/ControlPanel.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex-5 flexbox">
     <cp--action-buttons></cp--action-buttons>
-    <cp--flex-input-panel></cp--flex-input-panel>
+    <cp--flex-input-panel :flexItemGroup="flexItemGroup"></cp--flex-input-panel>
   </div>
 </template>
 
@@ -10,6 +10,7 @@ import eventBus from '../../../eventbus.js';
 import CP__ActionButtons from './CP__ActionButtons';
 import CP__FlexInputPanel from './CP__FlexInputPanel';
 export default {
+  props: ['flexItemGroup'],
   components: {
     'cp--action-buttons': CP__ActionButtons,
     'cp--flex-input-panel': CP__FlexInputPanel
