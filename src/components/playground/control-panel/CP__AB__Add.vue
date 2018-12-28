3c0fb17091b196a4da3f9b65972e50c0f7ba1770
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-27 23:48:19 -0600

    updated - custom button styling

--- src/components/playground/control-panel/CP__AB__Add.vue
+++ src/components/playground/control-panel/CP__AB__Add.vue
@@ -13,6 +13,18 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+button {
+  width: auto;
+  background: linear-gradient(to bottom, rgb(175, 240, 184), rgb(117, 170, 159));
+  border: 2px solid var(--backgroundGrey);
+  border-radius: 5px;
+  padding: .5rem;
+  font-family: 'Dosis', sans-serif;
+  font-size: .9rem;
+  font-weight: 600;
+}
+button:active {
+  background: linear-gradient(to top, rgb(175, 240, 184), rgb(117, 170, 159));
+}
 </style>
