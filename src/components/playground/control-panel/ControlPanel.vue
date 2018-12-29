4bc92e00fd29ab915e774d8ca646fbb22301fe9d
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-28 20:53:25 -0600

    updated - passing flexdir through to child cmp

--- src/components/playground/control-panel/ControlPanel.vue
+++ src/components/playground/control-panel/ControlPanel.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="flex-5 flexbox">
-    <cp--action-buttons></cp--action-buttons>
-    <cp--flex-input-panel :flexItemGroup="flexItemGroup"></cp--flex-input-panel>
+    <cp--action-buttons
+      :flexGroupDirection="flexGroupDirection">
+    </cp--action-buttons>
+    <cp--flex-input-panel 
+      :flexItemGroup="flexItemGroup">
+    </cp--flex-input-panel>
   </div>
 </template>
 
@@ -10,7 +14,7 @@ import eventBus from '../../../eventbus.js';
 import CP__ActionButtons from './CP__ActionButtons';
 import CP__FlexInputPanel from './CP__FlexInputPanel';
 export default {
-  props: ['flexItemGroup'],
+  props: ['flexItemGroup', 'flexGroupDirection'],
   components: {
     'cp--action-buttons': CP__ActionButtons,
     'cp--flex-input-panel': CP__FlexInputPanel
