624fd6e619b059ed81fca38f5cfbff7584dc8510
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-28 20:51:41 -0600

    updated - reactivity for flex amounts, input panel

--- src/components/playground/control-panel/CP__FlexAmountInput.vue
+++ src/components/playground/control-panel/CP__FlexAmountInput.vue
@@ -1,13 +1,49 @@
 <template>
-  
+  <div class="flex-amount-input-group">
+    <label for="flex-amount">Flex Item {{ index + 1 }}</label>
+    <input 
+      @input="changeFlexAmount"
+      v-model="newFlexAmount"
+      type="number" 
+      name="flex-amount"
+      min="1"
+      max="20">
+  </div>
 </template>
 
 <script>
+import eventBus from '../../../eventbus.js'
 export default {
-
+  data () {
+    return {
+      newFlexAmount: 1
+    }
+  },
+  props: ['flexItem', 'index'],
+  methods: {
+    changeFlexAmount() {
+      const flexData = {
+        index: this.index,
+        flexAmount: this.newFlexAmount
+      }
+      eventBus.$emit('changeFlexAmount', flexData)
+    }
+  }
 }
 </script>
 
 <style>
-
+.flex-amount-input-group label {
+  display: block;
+  font-weight: 500;
+}
+.flex-amount-input-group input {
+  width: 6rem;
+  padding: .1rem .25rem;
+  border: 2px solid var(--backgroundGrey);
+  border-radius: 3px;
+  font-family: 'Dosis', sans-serif;
+  font-size: .9rem;
+  font-weight: 500;
+}
 </style>
--- src/components/playground/control-panel/CP__FlexInputPanel.vue
+++ src/components/playground/control-panel/CP__FlexInputPanel.vue
@@ -1,11 +1,20 @@
 <template>
-  <div class="flex-4 flexbox">
-    
+  <div class="flex-6 flexbox flex-align-center flexgap-2 flex-wrap">
+    <cp--flex-amount-input 
+      v-for="(flexItem, i) in flexItemGroup" 
+      :key="i"
+      :flexItem="flexItem"
+      :index="i"></cp--flex-amount-input>
   </div>
 </template>
 
 <script>
+import CP__FlexAmountInput from './CP__FlexAmountInput';
 export default {
+  props: ['flexItemGroup'],
+  components: {
+    'cp--flex-amount-input': CP__FlexAmountInput
+  }
 
 }
 </script>
