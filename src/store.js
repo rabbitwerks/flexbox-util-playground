90f6722c968b74534da54582027ad7bd2d784f5f
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-28 23:01:04 -0600

    updated - mutation/action for flex-direction

--- src/store.js
+++ src/store.js
@@ -14,32 +14,44 @@ const store = new Vuex.Store({
     flexGroupDirection: 'flexdir-row',
   },
   getters: {
+    // gets entire flex-item-group
     getFlexGroup: state => state.flexItemGroup,
     getFlexGroupItem: state => index => state.flexItemGroup[index],
+    getFlexDirection: state => state.flexGroupDirection,
   },
   mutations: {
-    addItemToGroup_STORE(state) {
+    addItemToGroup_MUTA(state) {
       console.log('mutation');
       state.flexItemGroup.push({
         flex: 1, customWidth: '',
       });
     },
-    removeItemFromGroup_STORE(state, index) {
+    removeItemFromGroup_MUTA(state, index) {
+      if (state.flexItemGroup.length === 0) {
+        console.log('no flex items in the group');
+        return;
+      }
       if (index) {
         state.flexItemGroup.splice(index, 1);
       } else {
         state.flexItemGroup.splice(state.flexItemGroup.length - 1, 1);
       }
     },
+    setFlexDirection_MUTA(state, newDirection) {
+      state.flexGroupDirection = newDirection;
+    },
   },
   actions: {
     addItemToGroup_STORE(context) {
       console.log('action');
-      context.commit('addItemToGroup_STORE');
+      context.commit('addItemToGroup_MUTA');
     },
     removeItemFromGroup_STORE(context, index) {
-      context.commit('removeItemFromGroup_STORE', index);
-    }
+      context.commit('removeItemFromGroup_MUTA', index);
+    },
+    setFlexDirection_STORE(context, newDirection) {
+      context.commit('setFlexDirection_MUTA', newDirection);
+    },
   },
 });
 
