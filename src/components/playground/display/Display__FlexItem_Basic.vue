2e1b95a7a23ceb968d3a6a5d82541dc8dba3f07b
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-29 23:22:48 -0600

    updated - flex item inner, transition, vuex store

--- src/components/playground/display/Display__FlexItem_Basic.vue
+++ src/components/playground/display/Display__FlexItem_Basic.vue
@@ -1,16 +1,42 @@
 <template>
   <div 
-    :style="{ 'flex': flexItem.flex }"
+    :style="setStyle"
     @dblclick="removeSelf"
-    class="flex-item--basic flexbox-space-center">
-    <span>Flex: {{ flexItem.flex }}</span>
+    class="flex-item--basic">
+    <div class="flex-item--basic--inner flexbox-space-center">
+      <span v-if="!flexItem.isCustomWidth">
+        Flex Amount: {{ flexItem.flex }}
+      </span>
+      <span 
+        v-else-if="flexItem.measurementUnits === 'px' 
+        && flexItem.customWidth >= 100"> 
+        Width: {{ getCustomWidth }}
+      </span>
+      <span 
+        v-else-if="flexItem.measurementUnits === 'rem' 
+        && flexItem.customWidth >= 5"> 
+        Width: {{ getCustomWidth }}
+      </span>
+    </div>
   </div>
+
 </template>
 
 <script>
 import eventBus from '../../../eventbus.js'
 export default {
   props: ['flexItem', 'index'],
+  computed: {
+    getCustomWidth() {
+      return this.$store.getters.getFlexGroupItem(this.index).customWidth + this.$store.getters.getFlexGroupItem(this.index).measurementUnits
+    },
+    setStyle() {
+      if (this.flexItem.isCustomWidth) {
+        return { 'width': this.getCustomWidth }
+      }
+      return { 'flex': this.flexItem.flex }
+    }
+  },
   methods: {
     removeSelf() {
       eventBus.$emit('removeSelfFromGroup', this.index)
@@ -19,10 +45,14 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .flex-item--basic {
   height: 100%;
   background-color: var(--mainTurq);
-  border: 2px solid var(--backgroundGrey)
+  border: 2px solid var(--backgroundGrey);
+  transition: flex .2s ease-in-out, width .2s ease-in-out;
+}
+.flex-item--basic--inner {
+  height: 100%;
 }
 </style>
